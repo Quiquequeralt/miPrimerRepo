9df232a2f27d7f6663293383cdf6061ad3f7ca3f
Quique <enrsan.alu@iesbenigaslo.es>
2022-03-16 13:54:50 +0100

    Añadida una rama

--- Cliente.java
+++ Cliente.java
@@ -3,11 +3,13 @@ public class Cliente {
 	private String dni;
 	private String nombre;
 	private String especialidad;
+	private String pais;
 
-	public Cliente(String dni, String nombre, String especialidad) {
+	public Cliente(String dni, String nombre, String especialidad,String pais) {
 		this.dni = dni;
 		this.nombre = nombre;
 		this.especialidad = especialidad;
+		this.pais = pais;
 	}
 
 	public String getDni() {
